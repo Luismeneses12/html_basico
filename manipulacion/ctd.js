7af278868422a0cbeae85415651fa3e2119ed88c
lmeneses <luis.0495@hotmail.com>
2025-04-27 00:09:43 -0500

    refator se modifico  el nombre de una variable y se hizo el sonido final para le contador

--- manipulacion/ctd.js
+++ manipulacion/ctd.js
@@ -1,12 +1,18 @@
 const p= document.getElementById("numero")
 const aumnetar = document.getElementById("aumentar")
 const disminuir = document.getElementById("disminuir")
-const restaurar = document.getElementById("resturar")
- let contador = 0 ;
+const restaurar = document.getElementById("restaurar")
+// sonidos 
+const sonidoAumnetar = new Audio("aumentar.mp3") 
+const sonidoDisminuir = new Audio("disminuir.mp3")
+const sonidoRestaurar = new Audio("restaurar.mp3")
+let contador = 0 ;
 
  aumnetar.addEventListener("click",()=>{
     if (contador <10 ){
-     contador ++;}    
+     contador ++;
+     sonidoAumnetar.play()  }
+     
     p.textContent= contador 
     if(contador > 0){ 
     p.style.color="black"
@@ -18,6 +24,7 @@ const restaurar = document.getElementById("resturar")
     if (contador > 0 ){
     resta =contador --;
     p.textContent=resta
+    sonidoDisminuir.play()
     }
     if( contador === 0 ){
     p.style.color="red"
@@ -27,6 +34,7 @@ const restaurar = document.getElementById("resturar")
         contador = 0;
         p.textContent= contador
         p.style.color="red"
+        sonidoRestaurar.play()
         
     })
 // estilos en pagina principal con js
